e8e987c1ed1c975f7971a577d0e18fb52fc322eb
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-06-24 16:19:55 -0400

    added more link data, working on styling

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const boxes = document.querySelectorAll('.bootstrap-box');
-
-// function handleBoxClick(event) {
-//     alert(`You clicked on ${event.target.id}!`);
-// }
-
-// boxes.forEach(box => {
-//     box.addEventListener('click', handleBoxClick);
-// });
-
-const box1 = document.getElementById('box-1');
-box1.addEventListener('click', () => {
-    window.open('http://mywb.me/web', '_blank').focus();
-});
-
-navigateTo(url) {
+function navigateTo(url) {
     window.open(url, '_blank').focus();
 }
